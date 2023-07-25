cc82ebf099144ab14b29ea9c7c5394c3984a587d
MUJIMERGE <smaryn101@gmail.com>
2023-07-25 22:24:00 +0900

    [PWSBACK1][MS2] SpringConfig 파일 정의

--- src/main/java/com/smaryn/pws/back/member/service/MemberService.java
+++ src/main/java/com/smaryn/pws/back/member/service/MemberService.java
@@ -1,4 +1,13 @@
 package com.smaryn.pws.back.member.service;
 
+import com.smaryn.pws.back.member.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+
+@Transactional
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 }
